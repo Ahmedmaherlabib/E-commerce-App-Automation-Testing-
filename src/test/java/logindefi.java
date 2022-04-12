5f4afe145930fca25c32a16915e62ec0fbb944dd
Ahmedmaherlabib <99432147+Ahmedmaherlabib@users.noreply.github.com>
2022-04-12 19:20:36 +0200

    update

---
+++ src/test/java/logindefi.java
@@ -0,0 +1,10 @@
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+
+public class logindefi {
+     @Given("user navigate to login page")
+    public void user_open_browser(){
+}
+    @And("user click on login button")
+    public void user_press_button(){}
+}
