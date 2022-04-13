ccff7e5a04c7e4ec42f520eaedb7989c7f4f7f1a
Ahmedmaherlabib <99432147+Ahmedmaherlabib@users.noreply.github.com>
2022-04-13 03:47:29 +0200

    update1

---
+++ src/main/resources/Features/registration.feature
@@ -0,0 +1,18 @@
+Feature: registration user should be able to sign up with new accounts
+
+
+    Scenario:user could sign up with valid data
+      Given user open chrome driver
+      And user navigate to home page
+      And user press registration link
+      When user fills Your Personal Details with valid data
+      #And user fills Your Password Details with valid data
+      #And user press on register button
+      #Then user register successfully
+      And user quite driver
+
+
+    Scenario:
+
+
+    Scenario:
---
+++ src/test/java/stepDefinition/registration.java
@@ -0,0 +1,42 @@
+package stepDefinition;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import io.github.bonigarcia.wdm.managers.FirefoxDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class registration {
+    WebDriver driver;
+    @Given("user open chrome driver")
+    public void open_chrome(){
+        WebDriverManager.chromedriver().setup();
+         driver=new ChromeDriver();
+         driver.manage().window().maximize();
+
+    }
+    @And("user navigate to home page")
+    public void navigate_home() {
+        WebDriverManager.chromedriver().setup();
+        driver.navigate().to("https://demo.nopcommerce.com/");
+    }
+    @And("user press registration link")
+    public void click_regis_link(){
+        driver.findElement(By.className("ico-register")).click();
+    }
+    @And("user quite driver")
+    public void quite_driver(){
+        driver.quit();
+    }
+    @When("user fills Your Personal Details with valid data")
+    public void personal_info(){
+        driver.findElement(By.id("FirstName")).sendKeys("automation");
+        driver.findElement(By.id("LastName")).sendKeys("tester");
+
+
+    }
+}
