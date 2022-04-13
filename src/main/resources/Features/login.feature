715ae269c7c8f7f96db30deaa20679f62a954ee3
Ahmedmaherlabib <99432147+Ahmedmaherlabib@users.noreply.github.com>
2022-04-13 16:04:15 +0200

    update1

--- src/main/resources/Features/login.feature
+++ src/main/resources/Features/login.feature
@@ -2,7 +2,9 @@ Feature: user should be able to login to the system
 
 
     Scenario: user login with valid username and password
-      Given user navigate to login page
+      Given user open chrome driver
+      And user navigate to home page
+      And user press login link
       When user enter valid username and password
       And user click on login button
       #expected result
--- src/test/java/stepDefinition/registration.java
+++ src/test/java/stepDefinition/registration.java
@@ -1,66 +1,50 @@
 package stepDefinition;
-
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
-import io.github.bonigarcia.wdm.managers.FirefoxDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.asserts.SoftAssert;
-
 import java.util.concurrent.TimeUnit;
 
 public class registration {
-    WebDriver driver;
-    @Given("user open chrome driver")
-    public void open_chrome(){
-        WebDriverManager.chromedriver().setup();
-         driver=new ChromeDriver();
-         driver.manage().window().maximize();
-         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-    }
     @And("user navigate to home page")
     public void navigate_home() {
         WebDriverManager.chromedriver().setup();
-        driver.navigate().to("https://demo.nopcommerce.com/");
+            Hooks.driver.navigate().to("https://demo.nopcommerce.com/");
     }
     @And("user press registration link")
     public void click_regis_link(){
-        driver.findElement(By.className("ico-register")).click();
+        Hooks.driver.findElement(By.className("ico-register")).click();
     }
-    @And("user quite driver")
-    public void quite_driver(){
-        driver.quit();
-    }
+
     @When("user fills Your Personal Details with valid data")
     public void personal_info(){
-        driver.findElement(By.id("FirstName")).sendKeys("automation1");
-        driver.findElement(By.id("LastName")).sendKeys("tester2");
-        driver.findElement(By.id("Email")).sendKeys("test2@example.com");
+        Hooks.driver.findElement(By.id("FirstName")).sendKeys("automation1");
+        Hooks.driver.findElement(By.id("LastName")).sendKeys("tester2");
+        Hooks.driver.findElement(By.id("Email")).sendKeys("test3@example.com");
     }
     @And("user fills Your Password Details with valid data")
     public void password(){
-        driver.findElement(By.id("Password")).sendKeys("password");
-        driver.findElement(By.id("ConfirmPassword")).sendKeys("password");
+        Hooks.driver.findElement(By.id("Password")).sendKeys("password");
+        Hooks.driver.findElement(By.id("ConfirmPassword")).sendKeys("password");
     }
     @And("user press on register button")
     public void press_register_button(){
-        driver.findElement(By.id("register-button")).click();
+        Hooks.driver.findElement(By.id("register-button")).click();
     }
     @Then("user register successfully")
     public void register_successfully()  {
         //driver.findElement(By.className("result")).isDisplayed();
 
         SoftAssert softAssert=new SoftAssert();
-        softAssert.assertTrue(driver.findElement(By.className("result")).isDisplayed());
+        softAssert.assertTrue(  Hooks.driver.findElement(By.className("result")).isDisplayed());
         softAssert.assertAll();
 
 
     }
-}
+    }
